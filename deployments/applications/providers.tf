238286e8af9e49b33875a5db78c67cf9d0d4bdf7
Jasper Ginn <jasperginn@gmail.com>
2025-11-01 13:04:55 +0100

    build: update bootstrap

---
+++ deployments/applications/providers.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    nomad = {
+      source  = "hashicorp/nomad"
+      version = "~>2.5.0"
+    }
+    vault = {
+      source  = "hashicorp/vault"
+      version = "~>5.3.0"
+    }
+    minio = {
+      source  = "aminueza/minio"
+      version = "~>3.8.0"
+    }
+    consul = {
+      source  = "hashicorp/consul"
+      version = "~>2.22.0"
+    }
+  }
+}
+
+provider "nomad" {}
+
+provider "vault" {}
+
+provider "consul" {
+  # NB: provider does not read address from env var
+  address    = "localstack.local:8500"
+  datacenter = "localstack"
+}
